7313d0a55184525903ae2b5d632e762842d66f2b
yalematta <layale.matta@gmail.com>
2018-01-21 00:31:16 +0200

    sample committed :sparkling_heart:

---
+++ app/src/main/java/com/yalematta/android/androidwidget/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.yalematta.android.androidwidget.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yalematta.android.androidwidget.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
